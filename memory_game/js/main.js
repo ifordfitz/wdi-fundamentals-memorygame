1e485902ffd170f7bf33b51fbb0a4eb6f9c7e290
Ian Fitzgerald <ianfitzgerald@michael-fitzgeralds-imac-2.local>
2019-03-15 18:12:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,26 +24,40 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+      alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+      alert("Sorry, try again.");
 }
 };
-var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId].rank);
+
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
+
     cardsInPlay.push(cards[cardId].rank);
     if (cardsInPlay.length === 2) {
       checkForMatch();
     }
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+// console.log(cards[cardId].cardImage);
+// console.log(cards[cardId].suit);
 
   };
 
-flipCard(0);
-flipCard(2);
+  var createBoard = function() {
+  	for (let i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+ createBoard();
+
+
 
 
 
